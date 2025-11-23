add8b0866dfbdb5708192ee960113f98a8aab78a
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-23 14:12:40 +0100

    feat: add GraphQL endpoint with depth limit validation and DataLoader context

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,20 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
+import { GraphQLError, GraphQLSchema, parse, validate, graphql } from 'graphql';
+import { createLoaders } from './types/dataLoader.js';
+import depthLimit from 'graphql-depth-limit';
+
+export const depthLimitValidate = (query: string, schema: GraphQLSchema, dLimit: number): GraphQLError | null => {
+  const document = parse(query);
+  const validationErrors = validate(schema, document, [depthLimit(dLimit)]);
+  const resError = validationErrors.find(
+    (error) => error.name === 'GraphQLError' && error.message.includes('exceeds maximum operation depth')
+  ) || null;
+  return resError;
+};
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  const { prisma } = fastify;
+  const { postsDataloader, memberTypesDataloader, profilesByIdDataloader, profilesDataloader } = await createLoaders(fastify);
 
   fastify.route({
     url: '/',
@@ -15,7 +26,37 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
+      const queryStr = req.body?.query;
+      const variablesStr = req.body?.variables;
+      const MAX_QUERY_DEPTH = 5;
+
+      const depthQueryError = depthLimitValidate(queryStr, schema, MAX_QUERY_DEPTH);
+
+      if(depthQueryError) {
+        const message = `exceeds maximum operation depth of ${ MAX_QUERY_DEPTH }`
+        return {
+          errors: [
+            {
+              message: message,
+            },
+          ],
+        }
+      }
+
+      const res = await graphql({
+        schema: schema,
+        source: queryStr,
+        variableValues: variablesStr,
+        contextValue: {
+          fastify,
+          postsDataloader,
+          profilesDataloader,
+          profilesByIdDataloader,
+          memberTypesDataloader
+        },
+      });
+
+      return { ...res };
     },
   });
 };
