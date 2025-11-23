1208f6615862e8e56a5a556a2c99f5562b7c525a
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-23 15:38:14 +0100

    feat: add develop branch

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -4,6 +4,7 @@ import { GraphQLError, GraphQLSchema, parse, validate, graphql } from 'graphql';
 import { createLoaders } from './types/dataLoader.js';
 import depthLimit from 'graphql-depth-limit';
 
+
 export const depthLimitValidate = (query: string, schema: GraphQLSchema, dLimit: number): GraphQLError | null => {
   const document = parse(query);
   const validationErrors = validate(schema, document, [depthLimit(dLimit)]);
